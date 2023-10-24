a5ec0c37e7321addab4c3e90c97dff2ba3f0c49e
Vladimir Berestnev <berestnew@yandex.ru>
2023-10-24 15:28:29 +0300

    2

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = `[
+{ "title":"Плавание","time":"12:00","max":"50","current":"10"},
+{ "title":"Аэробика","time":"15:30","max":"30","current":"18"},
+{ "title":"Футбол","time":"00:00","max":"70","current":"70"}
+]
+`
